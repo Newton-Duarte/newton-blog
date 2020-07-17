f966e7ea1c43ffb6d4b9a912d2e5efcfcd13998b
Newton Darte <newton_@hotmail.com>
2020-07-17 06:41:34 -0300

    Setup Vue.js project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/" class="logo">Newton's Blog</router-link>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: var(--main-box-shadow);
+}
+.logo {
+  font-size: 1.3rem;
+  font-weight: bold;
+  padding: 10px 0;
+}
+</style>
